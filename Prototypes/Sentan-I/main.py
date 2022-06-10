626fe8835251e1ca4d5beb01e4844ebe2f61a633
Luis Carlos Quesada Rodriguez <b65580@ECCI102L385804.ecci.ucr.ac.cr>
2022-06-10 14:09:40 -0600

    Transformaciones de Raw data

--- Prototypes/Sentan-I/main.py
+++ Prototypes/Sentan-I/main.py
@@ -1,3 +1,15 @@
+import pandas as pd
 from processing import *
-from Dias_Model import Dias_Model
-from Sentan_Model import Sentan_Model
+#from Dias_Model import Dias_Model
+#from Sentan_Model import Sentan_Model
+
+data = pd.read_pickle('data.pkl')
+data = data.drop(['File name', 'Duration'], axis=1)
+
+raw_y = data['Emotion'] 
+raw_x = data.drop(['Emotion'], axis=1)
+
+
+
+x = x_flat(raw_x)
+y = encode(raw_y)
