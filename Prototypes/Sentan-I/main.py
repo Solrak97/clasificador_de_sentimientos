6046deba2c5d74b22c39d039d8be9a484db52170
Solrak <luiscarlosquesada@gmail.com>
2022-06-23 17:21:01 -0600

    Metodos de pre-processing listos

--- Prototypes/Sentan-I/main.py
+++ Prototypes/Sentan-I/main.py
@@ -1,7 +1,7 @@
 from torch import nn
 from torch.optim import Adam
 import pandas as pd
-from pre_processing import *
+from pre_processing import to_tensor, to_labels, load_data, split
 from Dias_Model import Dias_Model
 
 from model_training import train
@@ -9,12 +9,15 @@ from Sentan_Model import Sentan_Model
 
 data_path = 'data.pkl'
 
-'''
-features, labels, classes = load_data(data_path)
-x_train, x_val, y_train, y_val = split_and_tensor(features, labels)
-'''
 
-'''
+
+features, labels, classes = load_data(data_path)
+#k_folds = split(features, labels)
+
+print(to_labels(labels, classes))
+
+
+
 EPOCHS = 700
 INIT_LR = 1e-3
 TRAIN = (x_train, y_train)
@@ -27,4 +30,3 @@ VERBOSE = True
 train(MODEL, EPOCHS, TRAIN, TEST, OPTIMIZER, LOSS_FN, VERBOSE)
 
 torch.save(MODEL.state_dict(), 'Prototypes/Sentan-I/SaveModel/base_model.pt')
-'''
--- Prototypes/Sentan-I/pre_processing.py
+++ Prototypes/Sentan-I/pre_processing.py
@@ -1,36 +1,45 @@
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold, train_test_split
 import torch
 import torch.nn.functional as F
 import pandas as pd
 import numpy as np
 
 
-# Carga los datos en formato Numpy
-def load_data(path):
-    data = pd.read_pickle(path)
-    features = to_numpy(data['Composite_Vector'])
-    data['Ordinal_Emotion'] = data['Ordinal_Emotion'].astype(int) - 1
-    ordinal_labels = data['Ordinal_Emotion']
-    label_encoding = data.groupby('Ordinal_Emotion')['Emotion'].unique()
-    ordinal_labels = ordinal_labels.to_numpy().astype(np.int64)
 
-    return features, ordinal_labels, label_encoding
-
-
-def split_and_tensor(x, y, t_size=0.2):
-    x = torch.unsqueeze(torch.tensor(x), dim=1).type(torch.float32)
-    y = torch.from_numpy(y)
-
-    y = F.one_hot(y, 8).type(torch.float32)
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=t_size)
-
-    return x_train, x_test, y_train, y_test
-
-
+# Version propia para convertir una columna de tipo vector en un unico vector numpy
 def to_numpy(x):
     data = np.array([])
     for v in x:
         data = np.concatenate((data, v), axis=0)
     data = data.reshape(-1, 193)
     return data
+
+
+# Carga los datos en formato Numpy
+def load_data(path):
+    data = pd.read_pickle(path)
+    features = to_numpy(data['Composite_Vector'])
+    ordinal_labels = data['Ordinal_Emotion']
+    label_encoding = data.groupby('Ordinal_Emotion')['Emotion'].unique()
+    ordinal_labels = ordinal_labels.to_numpy().astype(np.int64)
+    return features, ordinal_labels, label_encoding
+
+
+# Crea la división de los datos en k folds
+# El formato es una lista de listas
+# Se puede ver como [[Train], [Test]]
+def split(x, y, splits = 5, random_state = 0):
+    skf = StratifiedKFold(n_splits=splits, random_state = random_state)
+    return skf.split(x, y)
+
+
+# Codifica los valores de y en one hot encoding y transforma el vector
+# En un tensor apto para usarse en Torch
+def to_tensor(x, y):
+    x = torch.unsqueeze(torch.tensor(x), dim=1).type(torch.float32)
+    y = torch.from_numpy(y)
+    return x, y
+
+
+def to_labels(preds, labels):
+    return [labels[i][0] for i in preds]
