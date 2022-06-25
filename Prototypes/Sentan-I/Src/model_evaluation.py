d1e935ab739631312cd251e8f322e2da57046b3d
Solrak <luiscarlosquesada@gmail.com>
2022-06-25 01:10:39 -0600

    Corrección error en etiquetas de matriz de confusion

--- Prototypes/Sentan-I/Src/model_evaluation.py
+++ Prototypes/Sentan-I/Src/model_evaluation.py
@@ -6,12 +6,14 @@ import sklearn.metrics as metrics
 
 # Graficación de matríz de confusión
 def confussion_matrix(y_true, y_pred, classes):
+
     cf_matrix = metrics.confusion_matrix(y_true, y_pred)
+
     df_cm = pd.DataFrame(cf_matrix, index=[i[0] for i in classes],
                          columns=[i[0] for i in classes])
 
     plt.figure(figsize=(10, 7))
-    sns.heatmap(df_cm, annot=True)
+    sns.heatmap(df_cm, annot=True, fmt='g', cmap='YlOrBr')
 
     plt.show()
 
